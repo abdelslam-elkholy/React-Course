71e6bb79bdf86614f4d4487ba9bf85ebed1b296c
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-07 06:36:36 +0300

    intial commit

--- 03-pizza-menu/starter/pizza-menu/src/index.js
+++ 03-pizza-menu/starter/pizza-menu/src/index.js
@@ -4,3 +4,6 @@ import ReactDOM from "react-dom/client";
 function App() {
   return <h1>Welcome React</h1>;
 }
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<App />);
