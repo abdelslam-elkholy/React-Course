1a536bf6fdbb11166622a7df806e0dbef3b27efe
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-19 21:29:14 +0300

    startProject

--- 04-steps/steps/src/App.js
+++ 04-steps/steps/src/App.js
@@ -1,14 +1,30 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 export default function App() {
+  const { steps, setSteps } = useState(1);
   return (
     <div className="steps">
-      <div className="nymbers">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+      <div className="numbers">
+        <div className={steps >= 1 ? "active" : ""}>1</div>
+        <div className={steps >= 2 ? "active" : ""}>2</div>
+        <div className={steps >= 3 ? "active" : ""}>3</div>
       </div>
-      <p className="message">Hello</p>
+      <p className="message">
+        Step {steps}: {messages[steps - 1]}
+      </p>
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}></button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Prev
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Next
+        </button>
       </div>
     </div>
   );
