c28d2d4d13bb81c76374e0fc4d37e8a0a744f2e2
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-20 23:41:29 +0300

    update

--- 05-travel-list/trvel-list/src/App.js
+++ 05-travel-list/trvel-list/src/App.js
@@ -1,6 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
 ];
 
 export default function App() {
@@ -25,8 +26,21 @@ function Form() {
 function PackingList() {
   return (
     <div className="list">
-      <h3>what do you need for your form</h3>
+      <ul>
+        {initialItems.map((ele) => (
+          <Item item={ele} />
+        ))}
+      </ul>
     </div>
+  );
+}
+function Item({ item }) {
+  return (
+    <li>
+      <span>
+        {item.quantity} {item.description}
+      </span>
+    </li>
   );
 }
 
