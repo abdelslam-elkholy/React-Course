6d1e3a45c01f965a6eff5c31dbfe5c9b543abbe8
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-20 23:22:07 +0300

    update

--- 05-travel-list/trvel-list/src/App.js
+++ 05-travel-list/trvel-list/src/App.js
@@ -1,9 +1,32 @@
-export default App();
-{
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+    </div>
+  );
 }
 
-function Logo() {}
+function Logo() {
+  return <h1>Far Away</h1>;
+}
 
-function packingList() {}
+function Form() {
+  return <div className="add-form"> what do you need for your form</div>;
+}
 
-function stats() {}
+function packingList() {
+  return (
+    <div className="list">
+      <h3>what do you need for your form</h3>
+    </div>
+  );
+}
+
+function stats() {
+  return (
+    <footer>
+      <em>You Have x items</em>
+    </footer>
+  );
+}
