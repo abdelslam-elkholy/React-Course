54940c0108196561bb81a7028e5e404ced40b6ca
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-30 02:11:12 +0300

    update

--- 05-travel-list/trvel-list/src/App.js
+++ 05-travel-list/trvel-list/src/App.js
@@ -1,15 +1,9 @@
 import { useState } from "react";
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 1, packed: true },
-];
 
 export default function App() {
   const [items, setItems] = useState([]);
   const addNewItem = (item) => setItems([...items, item]);
-  const deleteItem = (id) =>
-    setItems((items) => items.filter((item) => item.id !== id));
+  const deleteItem = (id) => setItems(items.filter((item) => item.id !== id));
   return (
     <div className="app">
       <Logo />
